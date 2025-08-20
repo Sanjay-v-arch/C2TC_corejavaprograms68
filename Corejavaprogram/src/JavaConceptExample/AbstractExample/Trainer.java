966d5dccb0b5ed996d4c096bec91e86a6b7fe7f7
STUDENT <STUDENT@DESKTOP-43R2TFM>
2025-08-20 14:10:53 +0530

    ADDED

---
+++ Corejavaprogram/src/JavaConceptExample/AbstractExample/Trainer.java
@@ -0,0 +1,11 @@
+package JavaConceptExample.AbstractExample;
+
+public abstract class Trainer {
+	abstract void session();
+	abstract void checks();
+	public void display() {
+		System.out.println("TNS CAPGEMINI TRAINER");
+	}
+	
+
+}
